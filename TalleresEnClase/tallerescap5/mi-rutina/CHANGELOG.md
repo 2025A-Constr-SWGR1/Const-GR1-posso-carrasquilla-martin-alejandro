180709e82cbbef8a722da0ca454e7814de850d4e
Martín Posso <martin.posso@epn.edu.ec>
2025-08-02 21:34:53 -0500

    Taller 4 capitulo 5: Commit final añadiendo medición de rendimiento con interceptor

--- TalleresEnClase/tallerescap5/mi-rutina/CHANGELOG.md
+++ TalleresEnClase/tallerescap5/mi-rutina/CHANGELOG.md
@@ -2,3 +2,7 @@
 
 ## [v1.0.0] – 2025-08-02
 - Endpoints: GET /personal/disponibilidad, GET /personal/concentracion, POST & GET /personal/recordatorios
+
+
+## [v2.0.0] – 2025-08-03
+- Añadido GET /personal/energia (% de energía)
--- TalleresEnClase/tallerescap5/mi-rutina/src/main.ts
+++ TalleresEnClase/tallerescap5/mi-rutina/src/main.ts
@@ -2,6 +2,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { LogTimeInterceptor } from './common/interceptors/log-time.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -17,7 +18,7 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-
+  app.useGlobalInterceptors(new LogTimeInterceptor());
   await app.listen(3000);
   console.log('API corriendo en http://localhost:3000');
   console.log('Swagger UI en http://localhost:3000/api');
