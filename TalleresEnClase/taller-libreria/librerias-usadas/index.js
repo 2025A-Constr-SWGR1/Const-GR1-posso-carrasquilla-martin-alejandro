0591be494ad9adec7b95251b038e69110d123724
Martín Posso <martin.posso@epn.edu.ec>
2025-08-03 15:23:22 -0500

    Implementando el uso de la libreria resta en el taller de libreria

--- TalleresEnClase/taller-libreria/librerias-usadas/index.js
+++ TalleresEnClase/taller-libreria/librerias-usadas/index.js
@@ -1,8 +1,11 @@
 const paqueteSuma = require('2025a-swgr1-mapc-suma');
 const paqueteMultiplicacion = require('2025a-swgr1-enmo-multiplicacion');
+const paqueteResta = require('2025a-swgr1-smvv-resta')
 
 const respuestaSuma = paqueteSuma.suma(1, 2);
 const respuestaMult = paqueteMultiplicacion.multiplicacion(3, 5);
+const respuestaResta = paqueteResta.resta(7,2)
 
 console.log('La respuesta de la suma es: ', respuestaSuma);
 console.log('La respuesta de la multiplicacion es: ', respuestaMult);
+console.log('La resputesa de la resta es: ', respuestaResta);
